d045e715cd0d96437c4ce570da25c5796dbc4d6a
Jesper Munkeby <munkeby.j@gmail.com>
2023-04-17 23:16:21 +0200

    initial setup

---
+++ GossipPOC/GossipPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  GossipPOCApp.swift
+//  GossipPOC
+//
+//  Created by Jesper Munkeby on 2023-04-17.
+//
+
+import SwiftUI
+
+@main
+struct GossipPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
