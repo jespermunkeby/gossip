7c31330ab72a24366c8b1fe930a1655054bcb885
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-05-03 12:31:31 +0200

    Settings view for authentication

--- App/Gossip/Gossip/View/Settingsiew.swift
+++ App/Gossip/Gossip/View/Settingsiew.swift
@@ -1,20 +1,71 @@
 //
-//  Settingsiew.swift
+//  SettingsView.swift
 //  Gossip
 //
-//  Created by Abbas Alubeid on 2023-05-03.
+//  Created by Abbas Alubeid on 2023-05-02.
 //
 
 import SwiftUI
 
-struct Settingsiew: View {
+struct SettingsView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        ZStack {
+            VStack {
+                HeaderView(showSettings: .constant(false))
 
-struct Settingsiew_Previews: PreviewProvider {
-    static var previews: some View {
-        Settingsiew()
+                VStack(alignment: .leading, spacing: 30) {
+                    Group {
+                        Text("To authenticate yourself, please follow the steps below:")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .padding(.horizontal)
+                        
+                        Text("1. Connect to the Hub wifi.")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .padding(.horizontal)
+                        
+                        Text("2. After connecting, click the link below to authenticate.")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .padding(.horizontal)
+                        
+                        Link(destination: URL(string: "https://www.example.com")!) {
+                            HStack {
+                                Text("Authenticate")
+                                    .underline()
+                                    .foregroundColor(.blue)
+                            }
+                            .font(.title2)
+                            .padding(.horizontal)
+                        }
+                    }
+                    Spacer()
+                }
+                .padding(.top)
+            }
+            .navigationBarBackButtonHidden(true)
+
+            HStack {
+                Spacer()
+                Button(action: {
+                    self.presentationMode.wrappedValue.dismiss()
+                }) {
+                    HStack {
+                        Image(systemName: "arrow.uturn.left")
+                            .imageScale(.large)
+                            .scaleEffect(1.5, anchor: .center)
+                    }
+                    .padding(25)
+                    .background(Color(red: 0.7960784314, green: 0.8980392157, blue: 0.8745098039))
+                    .foregroundColor(.black)
+                    .clipShape(Circle())
+                }
+                .padding()
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
+        }
     }
 }
