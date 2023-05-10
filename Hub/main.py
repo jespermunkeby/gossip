65180dc1c07b4af2df852bdbab1d168e634597d8
vilmelhina <vilhelmina2.0@gmail.com>
2023-05-10 13:23:27 +0200

    Settings are now applied to central and peripheral processes

--- Hub/main.py
+++ Hub/main.py
@@ -20,60 +20,98 @@ def init_log():  # adapted from https://stackoverflow.com/a/56369583
         datefmt='%Y-%m-%d %H:%M:%S')
 
 
-def update_posts():
-    """ Reads posts from the database, and updates peripheral to broadcast those posts. """
-    posts = database.get_posts(content_only=True)
-    print("* broadcasting: " + str(posts))
-    peripheral.set_posts(posts)
-    new_posts_event.set()
+class Main:
+
+    def __init__(self):
+        print("q: quit, c: clear database, write anything else to add message")
+        init_log()
+        self.quit_event = threading.Event()          # event telling threads to finish up to exit program
+        self.new_posts_event = threading.Event()     # event telling peripheral to load new posts to advertise
+        self.database = post_database.PostDatabase()
+        self.settings = None
+        self.peripheral = None
+        self.peripheral_thread = None
+        self.central = None
+        self.central_thread = None
+        self.webb_app = None
+        self.web_thread = None
+
+    def start_peripheral(self):
+        self.peripheral = ble_peripheral.Peripheral(self.quit_event, self.new_posts_event,
+                                                    hub_name=self.settings["hub_name"]["value"])
+        self.update_posts()  # add posts to peripheral to broadcast
+        self.new_posts_event.clear()  # clear event, no need for it at start
+        self.peripheral_thread = threading.Thread(target=self.peripheral.advertise, args=())
+        self.peripheral_thread.start()
+
+    def start_central(self):
+        self.central = ble_central.Central(self.add_post, central_active=self.settings["rcv_posts"]["value"], quit_event=self.quit_event)
+        self.central_thread = threading.Thread(target=self.central.run, args=())
+        self.central_thread.start()
+
+    def update_posts(self):
+        """ Reads posts from the database, and updates peripheral to broadcast those posts. """
+        posts = self.database.get_posts(content_only=True)
+        print("* broadcasting: " + str(posts))
+        if self.peripheral:
+            self.peripheral.set_posts(posts)
+            self.new_posts_event.set()
+
+    def add_post(self, post):
+        """ Adds post to database and calls update_posts(). """
+        self.database.add_post(post)
+        self.update_posts()
+
+    def run_web_config(self):
+        self.webb_app = create_app(self.update_posts, self.settings_updated)
+        self.webb_app.run(host=WEB_APP_IP, port=WEB_APP_PORT, debug=False, use_reloader=False)
+
+    def start_web_thread(self):
+        self.web_thread = threading.Thread(target=self.run_web_config, args=())
+        self.web_thread.start()
+
+    def settings_updated(self):
+        self.settings = read_config()
+
+        # quit threads
+        self.quit_event.set()
+        self.peripheral_thread.join()
+        if self.central_thread:
+            self.central_thread.join()
+        self.quit_event.clear()
+
+        # restart threads
+        self.start_peripheral()
+        if self.settings["rcv_posts"]["value"]:
+            self.start_central()
+        else:
+            self.central = None
+            self.central_thread = None
+
+    def run(self):
+        self.settings = read_config()
+        self.start_peripheral()
+        if self.settings["rcv_posts"]["value"]:
+            self.start_central()
+        self.start_web_thread()
+        # user input loop
+        while True:
+            user_input = input()
+            if user_input == 'q':  # temporary (?), user enters q to quit
+                print("exiting...")
+                self.quit_event.set()  # set quit event, should get threads to finish
+                if self.peripheral_thread:
+                    self.peripheral_thread.join()  # wait for peripheral thread to finish
+                if self.central_thread:
+                    self.central_thread.join()
+                break
+            elif user_input == 'c':  # temporary (?), user enters c to clear database
+                print("* cleared database")
+                self.database.clear()
+            elif user_input != '':  # temporary (?), user enters post to add
+                self.add_post(user_input)
 
 
-def add_post(post):
-    """ Adds post to database and calls update_posts(). """
-    database.add_post(post)
-    update_posts()
-
-
-def run_web_config():
-    app = create_app(update_posts)
-    app.run(host=WEB_APP_IP, port=WEB_APP_PORT, debug=True, use_reloader=False)
-
-
-print("q: quit, c: clear database, write anything else to add message")
-init_log()
-
-quit_event = threading.Event()              # event telling threads to finish up to exit program
-new_posts_event = threading.Event()         # event telling peripheral to load new posts to advertise
-database = post_database.PostDatabase()
-settings = read_config()
-
-"""Initialize peripheral and central with current settings"""
-peripheral = ble_peripheral.Peripheral(quit_event, new_posts_event, hub_name = settings["hub_name"]["value"])
-central = ble_central.Central(add_post, central_active = settings["rcv_posts"]["value"])
-
-update_posts()              # add posts to peripheral to broadcast
-new_posts_event.clear()     # clear event, no need for it at start
-
-# create and run thread for peripheral
-peripheral_thread = threading.Thread(target=peripheral.advertise, args=())
-central_thread = threading.Thread(target=central.run, args=())
-web_thread = threading.Thread(target=run_web_config, args=())
-
-peripheral_thread.start()
-central_thread.start()
-web_thread.start()
-
-# user input loop
-while True:
-    user_input = input()
-    if user_input == 'q':           # temporary (?), user enters q to quit
-        print("exiting...")
-        quit_event.set()            # set quit event, should get threads to finish
-        peripheral_thread.join()    # wait for peripheral thread to finish
-        central_thread.join()
-        break
-    elif user_input == 'c':         # temporary (?), user enters c to clear database
-        print("* cleared database")
-        database.clear()
-    elif user_input != '':          # temporary (?), user enters post to add
-        add_post(user_input)
+if __name__ == "__main__":
+    main = Main()
+    main.run()
